07d86b3ff4a2c7cd0d2faa0de2c6944a85813b94
milonroy26 <milonchandro35@gmail.com>
2023-07-28 02:22:20 +0600

    token add local storage

--- src/component/Product-list.jsx
+++ src/component/Product-list.jsx
@@ -4,32 +4,32 @@ import fetchProduct from "../utils/fetchProduct";
 import { useState } from "react";
 
 const ProductList = () => {
-    const [products, setProducts] = useState([]);
-    const [error, setError] = useState('');
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
 
-    useEffect( () => {
-        fetchProduct()
-        .then( (data) => setProducts(data) )
-        .catch((eror) => console.log(eror))
-    },[]);
-    
-    // decide what to Render
-    let output;
-    if(error){
-        output = <div>There was an error</div>
-    }
-    else if(products?.length > 0){
-        output = products.map((product) => <Product key={product.id}></Product>)
-    }
-    else{
-        <div>NO Product Found</div>
-    }
-    return (
-        <div className="container z-10 mx-auto my-12 p-9">
-            <div className="grid grid-cols-1 mt-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-               {output}
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetchProduct()
+      .then((data) => setProducts(data))
+      .catch((eror) => console.log(eror));
+  }, []);
+
+  // decide what to Render
+  let output;
+  if (error) {
+    output = <div>There was an error</div>;
+  } else if (products?.length > 0) {
+    output = products.map((product) => (
+      <Product key={product.id} products={product}></Product>
+    ));
+  } else {
+    <div>NO Product Found</div>;
+  }
+  return (
+    <div className="container z-10 mx-auto my-12 p-9">
+      <div className="grid grid-cols-1 mt-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {output}
+      </div>
+    </div>
+  );
 };
 export default ProductList;
